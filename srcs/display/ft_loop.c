ebab4442f9e8b8f9a0e6e2b9ba2c4dc50a089110
aurbuche <buchau23@gmail.com>
2020-08-10 09:29:13 +0200

    10/08 - 2

--- srcs/display/ft_loop.c
+++ srcs/display/ft_loop.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/21 13:47:43 by user42            #+#    #+#             */
-/*   Updated: 2020/07/24 10:31:50 by user42           ###   ########lyon.fr   */
+/*   Updated: 2020/08/10 09:15:22 by user42           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int			cub_loop(t_cub *cub)
     //     ft_move(cub);
     // if (cub->turn)
     //     ft_turn(cub);
-    if (!cub->end)
-        ft_raycasting(cub);
+    // if (!cub->end)
+    //     ft_raycasting(cub);
     // mlx_put_image_to_window(cub->mlx_ptr, cub->mlx_win, cub->screen.img, 0, 0);
 	return (1);
 }
